5b59170f53179c3f6fca08aba2f09c422301022c
= <gamefurkanmeclis@gmail.com>
2024-09-24 17:01:16 +0300

    some changes

---
+++ public/build/assets/FullPageLayout-Blvqv4mn.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-wUprH5bk.js";function a({children:r}){return t.jsx(t.Fragment,{children:r})}export{a as F};
--- public/build/assets/FullPageLayout-Cbf384QT.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-CR-42WEE.js";function a({children:r}){return t.jsx(t.Fragment,{children:r})}export{a as F};
